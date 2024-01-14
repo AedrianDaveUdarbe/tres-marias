a67f29215a42f0ce9593374b16645fd7d69a746c
Aedrian Dave Udarbe <aedrian529@gmail.com>
2024-01-14 18:51:48 +0800

    Revert "Merge pull request #4 from AedrianDaveUdarbe/feat/ian/login"
    This reverts commit d6e255cc65e7714a592e9e94634f5d1442c2e6b2, reversing
    changes made to 6673d2c70235c5ba1bf4eb9098eb42f6860788fd.

--- index.php
+++ index.php
@@ -8,35 +8,8 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- bootswatch theme -->
     <link rel="stylesheet" href="https://bootswatch.com/3/lumen/bootstrap.min.css">
-    <style>
-        .form-container {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-        }
-
-        .form-login {
-            max-width: 330px;
-            padding: 1rem;
-        }
-
-        .form-login input[type="email"] {
-            margin-bottom: -1px;
-            border-bottom-right-radius: 0;
-            border-bottom-left-radius: 0;
-        }
-
-        .form-login input[type="password"] {
-            margin-bottom: 10px;
-            border-top-left-radius: 0;
-            border-top-right-radius: 0;
-        }
-    </style>
 </head>
 
 <body>
@@ -91,22 +64,18 @@ session_start();
     }
     ?>
 
-
     <!--Login form-->
-    <div class="form-container">
-        <form class="form-login" action="index.php" method="post">
-            <h1 class="mb-3 fw-normal">LOGIN</h1>
-            <div class="form-group">
-                <label class="control-label" for="username">Username</label>
-                <input type="text" class="form-control" id="username" name="username">
-            </div>
-            <div class="form-group">
-                <label class="control-label" for="password">Password</label>
-                <input type="password" class="form-control" id="password">
-            </div>
-            <button type="submit" name="login" href="#" class="btn btn-primary">LOGIN</button>
-            <a href="registration.php" class="btn btn-link">Click here to register an account</a>
-            <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
+    <div>
+        <form action="index.php" method="post">
+            <h1>LOGIN</h1>
+
+            <label for="">Username</label>
+            <input type="text" name="username" id="" placeholder="Type your username"><br>
+
+            <label for="">Password</label>
+            <input type="password" name="password" id="" placeholder="Type your password"><br>
+            <button type="submit" name="login" class="btn btn-primary">Login</button>
+            <a href="registration.php">Click here to register an account</a>
         </form>
     </div>
 

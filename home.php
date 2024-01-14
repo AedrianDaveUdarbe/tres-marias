5a98f8c45976b074868c231175b1ed4456066324
ianlabicani <labicaniian@gmail.com>
2024-01-14 22:20:43 +0800

    link external css

--- home.php
+++ home.php
@@ -8,7 +8,13 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <title>Tres Marias</title>
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <style>
+    .navbar-brand-logo {
+      width: 50px;
+    }
+  </style>
 </head>
 <body>
 

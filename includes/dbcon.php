e04ceb15d2ce0f63e82b678d49dcc41395e5cc97
ianlabicani <labicaniian@gmail.com>
2024-01-14 12:38:08 +0800

    change dbname

--- includes/dbcon.php
+++ includes/dbcon.php
@@ -1,27 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        $servername="localhost";
-        $username= "root";
-        $password= "";
-        $dbname= "tres marias";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "tres_marias";
 
-        try
-        {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            //echo "connected successfuly";
-        }
-        catch (Exception $e){
-            echo "connection failed".$e->getMessage()."";
-        }
+        //echo "connected successfuly";
+    } catch (Exception $e) {
+        echo "connection failed" . $e->getMessage() . "";
+    }
     ?>
 </body>
+
 </html>

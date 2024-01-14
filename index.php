2447f804cf60a42e5c3548eb03e3eb9e080b6841
ianlabicani <labicaniian@gmail.com>
2024-01-14 21:53:01 +0800

    refactor UI for login

--- index.php
+++ index.php
@@ -2,36 +2,28 @@
 session_start();
 if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['login'])) {
 
-        try {
-            $username = $_POST["username"];
-            $password = $_POST["password"];
+  try {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-            //to check if the user input does not fill the input box
-            if (empty($username) || empty($password)) {
-                echo "All field is required";
-            } else {
+    $query = "SELECT * FROM users WHERE username=:username AND password=:password";
+    $query_run = $conn->prepare($query);
 
-                //to check if the  username and password is true
-                $query = "SELECT * FROM users WHERE username=:username AND password=:password";
-                $query_run = $conn->prepare($query);
+    $query_run->execute(
+      array(
+        ":username" => $username,
+        ":password" => $password
+      )
+    );
 
-                $query_run->execute(
-                    array(
-                        ":username" => $username,
-                        ":password" => $password
-                    )
-                );
+    $count = $query_run->rowCount();
+    $userdata = $query_run->fetch(PDO::FETCH_ASSOC);
 
-                $count = $query_run->rowCount();
-
-                //to get the data of the table
-                $userdata = $query_run->fetch(PDO::FETCH_ASSOC);
-
-                //to count if the the account exist
-                if ($count > 0) {
+    //to count if the the account exist
+    if ($count > 0) {
       $_SESSION["username"] = $userdata["username"];
-                    header("Location: home.php");
-                } else {
+      header("Location: home.php");
+    } else {
 ?>
       <script type="module">
         import {
@@ -42,30 +34,73 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['login'])) {
         removeErrorMssg(5000);
       </script>
 <?php
-            }
-        } catch (Exception $e) {
-            echo "error " . $e->getMessage();
-        }
     }
-    ?>
+  } catch (Exception $e) {
+    echo "error " . $e->getMessage();
+  }
+}
+?>
 
-    <!--Login form-->
-    <div>
-        <form action="index.php" method="post">
-            <h1>LOGIN</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-            <label for="">Username</label>
-            <input type="text" name="username" id="" placeholder="Type your username"><br>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- bootswatch theme -->
+  <link rel="stylesheet" href="https://bootswatch.com/3/lumen/bootstrap.min.css">
 
-            <label for="">Password</label>
-            <input type="password" name="password" id="" placeholder="Type your password"><br>
-            <button type="submit" name="login" class="btn btn-primary">Login</button>
-            <a href="registration.php">Click here to register an account</a>
-        </form>
-    </div>
+  <style>
+    .form-container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+    }
 
-    <!-- bootstrap js cdn -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    .form-login {
+      max-width: 500px;
+      min-width: 350px;
+      padding: 1rem;
+    }
+
+    .form-login input[type="email"] {
+      margin-bottom: -1px;
+      border-bottom-right-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+
+    .form-login input[type="password"] {
+      margin-bottom: 10px;
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="form-container">
+    <form class="form-login bg-light shadow-lg p-5 mb bg-body-tertiary rounded-5" action="index.php" method="post" id="login-form">
+      <h1 class="mb-3 fw-normal text-center">LOGIN</h1>
+      <div class="form-group">
+        <label class="control-label" for="username">Username</label>
+        <input type="text" class="form-control" id="username" name="username">
+      </div>
+      <div class="form-group">
+        <label class="control-label" for="password">Password</label>
+        <input type="password" class="form-control" id="password" name="password">
+      </div>
+      <button type="submit" name="login" href="#" class="btn btn-primary w-100" id="login">LOGIN</button>
+      <a href="registration.php" class="btn btn-link text-center d-block">Click here to register an account</a>
+      <p class="mt-5 mb-3 text-body-secondary text-center">© 2023-2024</p>
+    </form>
+  </div>
+
+  <!-- bootstrap js cdn -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
   <script>
     const usernameInpt = document.getElementById("username");

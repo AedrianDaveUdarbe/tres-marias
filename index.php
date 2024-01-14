f7a6bffa8199e7ee4eaa6830a671983c10577d88
ianlabicani <labicaniian@gmail.com>
2024-01-14 13:21:23 +0800

    add bootstrap js cdn

--- index.php
+++ index.php
@@ -81,5 +81,7 @@
             <a href="registration.php">Click here to register an account</a>
         </form>
     </div>
+    <!-- bootstrap js cdn -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>

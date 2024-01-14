f8ae88545b2ba58de81819e41e6c8556e84c0f5a
ianlabicani <labicaniian@gmail.com>
2024-01-14 22:21:37 +0800

    change session name

--- home.php
+++ home.php
@@ -65,8 +65,8 @@ session_start();
 
     <!--User's Name-->
     <?php
-        if (isset($_SESSION["name"])) {
-            echo "<h3>Login Success, Welcome " . $_SESSION["name"] . "</h3>";
+  if (isset($_SESSION["username"])) {
+    echo "<h3>Login Success, Welcome " . $_SESSION["username"] . "</h3>";
         } else {
             // Redirect to index.php if the name is not set in the session
             header("Location: index.php");

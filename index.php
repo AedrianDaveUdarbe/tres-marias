841b04d5427dcb7c7b3ec4df0efa95ac82a21c9b
ianlabicani <labicaniian@gmail.com>
2024-01-14 13:21:40 +0800

    change formating to prettier

--- index.php
+++ index.php
@@ -1,70 +1,68 @@
 <?php require("includes/dbcon.php");
-    session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- bootswatch theme -->
     <link rel="stylesheet" href="https://bootswatch.com/3/lumen/bootstrap.min.css">
 </head>
+
 <body>
 
-<?php if (isset($_SESSION['message'])) {
-    echo '<h2>' . $_SESSION['message'] . '</h2>';
+    <?php if (isset($_SESSION['message'])) {
+        echo '<h2>' . $_SESSION['message'] . '</h2>';
 
-    // Clear the session message to prevent it from showing again
-    unset($_SESSION['message']);
-}
-?>
-
-<?php  if (isset($_POST['login'])) {
-   try{
-    $username=$_POST["username"];
-    $password=$_POST["password"];
-
-    //to check if the user input does not fill the input box
-    if(empty($username) || empty($password)) {
-        echo"All field is required";
+        // Clear the session message to prevent it from showing again
+        unset($_SESSION['message']);
     }
-     else {
+    ?>
 
-    //to check if the  username and password is true
-    $query= "SELECT * FROM users WHERE username=:username AND password=:password";
-    $query_run= $conn->prepare($query);
+    <?php if (isset($_POST['login'])) {
+        try {
+            $username = $_POST["username"];
+            $password = $_POST["password"];
 
-    $query_run->execute(
-        array(
-            ":username"=> $username,
-            ":password"=> $password
-        )
-    );
+            //to check if the user input does not fill the input box
+            if (empty($username) || empty($password)) {
+                echo "All field is required";
+            } else {
 
-    $count = $query_run->rowCount();
+                //to check if the  username and password is true
+                $query = "SELECT * FROM users WHERE username=:username AND password=:password";
+                $query_run = $conn->prepare($query);
 
-    //to get the data of the table
-    $userdata = $query_run->fetch(PDO::FETCH_ASSOC);
+                $query_run->execute(
+                    array(
+                        ":username" => $username,
+                        ":password" => $password
+                    )
+                );
 
-    //to count if the the account exist
-    if($count > 0) {
+                $count = $query_run->rowCount();
 
-        //to get the user's name data
-        $_SESSION["name"] = $userdata["name"];
-        header("Location: home.php");
+                //to get the data of the table
+                $userdata = $query_run->fetch(PDO::FETCH_ASSOC);
+
+                //to count if the the account exist
+                if ($count > 0) {
+
+                    //to get the user's name data
+                    $_SESSION["name"] = $userdata["name"];
+                    header("Location: home.php");
+                } else {
+                    echo "username or password is wrong!";
+                }
+            }
+        } catch (Exception $e) {
+            echo "error " . $e->getMessage();
+        }
     }
-    else{
-        echo "username or password is wrong!";
-    }
-     
-}
-   }
-   catch(Exception $e){
-    echo "error ".$e->getMessage();
-    
-   }
-}
-?>
+    ?>
 
     <!--Login form-->
     <div>
@@ -76,12 +74,14 @@
 
             <label for="">Password</label>
             <input type="password" name="password" id="" placeholder="Type your password"><br>
-
-            <input type="submit" value="LOGIN" name="login"><br>
+            <button type="submit" name="login" class="btn btn-primary">Login</button>
             <a href="registration.php">Click here to register an account</a>
         </form>
     </div>
+
     <!-- bootstrap js cdn -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </body>
+
 </html>

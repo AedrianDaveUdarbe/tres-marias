32e27a42305724507838780b809253343dbdc7e2
Aedrian Dave Udarbe <aedrian529@gmail.com>
2024-01-15 17:48:13 +0800

    added the buy-now function

--- home.php
+++ home.php
@@ -11,10 +11,67 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Tres Marias</title>
   <link rel="stylesheet" href="./css/bootstrap.min.css">
+ 
   <style>
     .navbar-brand-logo {
       width: 50px;
     }
+    .img-container{
+      width: 250px;
+      height: 300px;
+    }
+    .img-container img{
+      width: 250px;
+      height: 300px;
+      object-fit: cover ;
+    }
+    .img-product{
+      width: 200px;
+      height: 200px;
+      object-fit: cover ;
+      border-radius: 20px;
+      border: 1px black solid;
+    }
+
+    .custom-margin{
+      margin-left: 20px;
+      margin-top: 20px;
+      margin-right: 20px;
+    }
+
+    .main-products{
+      background-color: #FCF3EB;
+      padding: 30px ;
+    }
+    .desc{
+            grid-gap: 10px;     
+            display: flex;
+        
+        }
+        input[type=submit]{
+            border-style: none;
+            background-color: none;
+            cursor: pointer;
+            color: blue;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            width : 200px;
+        }
+        .text-center{
+          margin-top: 50px;
+        }
+         .footer{
+          background-color: #76453B;
+         
+         }
+         .footer{
+            overflow: hidden;
+         }
+         .footer-img img{
+          width: 300px;
+          height: 300px;
+         }
   </style>
 
 </head>
@@ -78,39 +135,96 @@ session_start();
   ?>
 
   <!--Introduction-->
-  <div>
-    <h1>Welcome to Tres Marias <br> Cakes and Pastries</h1>
-    <p>Indulge in the Sweet Delights of Aparri, Cagayan</p><br><br>
+  <div class="row custom-margin">
+    <div class="col-sm-8">
+        <h1>Welcome to <span style="color:red">Tres Marias</span> <br> Cakes and Pastries</h1>
+        <p>Indulge in the Sweet Delights of Aparri, Cagayan</p><br><br>
 
-    <p>Discover the artistry and passion that defines Tres Marias Bakeshop. Nestled in the heart of Aparri, Cagayan, our bakeshop is a haven for those seeking exquisite treats and delightful moments.</p><br><br><br>
+        <p>Discover the artistry and passion that defines Tres Marias Bakeshop. Nestled in the heart of Aparri, Cagayan, our bakeshop is a haven for those seeking exquisite treats and delightful moments.</p><br><br><br>
+    </div class="col-sm-4">
+    <div class="img-container">
+        <img src="assets/imgs/kim-chu.png" alt="kim-chu with pandesal">
+    </div>
   </div>
 
   <!--Main Products-->
-  <div>
+  <div class="main-products">
     <p>OUR BEST SELLERS</p>
     <h3>Favorites</h3>
-    <a href="">Mangoe Supreme Cake</a><br>
-    <a href="">Mangoe Supreme Cake</a><br>
-    <a href="">Mangoe Supreme Cake</a><br>
-    <a href="">Mangoe Supreme Cake</a><br><br>
-  </div>
+    <div class="row justify-content-center">
+        <?php 
+         $product_pics = "SELECT * FROM product_pics WHERE productID <=5";
+         foreach($conn->query($product_pics) as $product_pic){
+             echo "<div class='col-sm-2 m-1'><img src='assets/imgs/".$product_pic['filename']."' placeholder='product-image' class='img-product' <br>";
 
+              $productID=$product_pic['productID'];
+              $products = "SELECT * FROM products WHERE productID =$productID";
+              $display = $conn->query($products);
+              $description = $display->fetch(PDO::FETCH_ASSOC);
+             
+             echo "<form action='product-page.php' method='get'>";
+             echo  "<input type='submit' value='".$description['name']."'name='product-name'><br>";
+             echo " <input type='hidden' name='productID' value=".$description['productID'].">";
+             echo "</form>";
+             echo "</div>";
+         }
+         
+        ?>
+
+    </div>
+  </div>
+        
   <div>
-    <h1>GET A CAKE</h1><br>
-    <h4>NEW AND IMPROVED</h4>
-    <h2>Yema Caramel Cake</h2>
-    <h4>Experience pure bliss with our updated Yema Caramel Cake - a revamped version of our classic favorite!</h4><br>
-
-    <button><a href="product.php">START ORDER</a></button>
+    <h1 class="text-center">GET A CAKE</h1><br>
+    
   </div>
-
+  
+  <div class="row m-5 justify-content-center">
+          <div class="col-sm-4">
+              <h4>NEW AND IMPROVED</h4>
+              <h2>Yema Caramel Cake</h2>
+              <h4>Experience pure bliss with our updated Yema Caramel Cake - a revamped version of our classic favorite!</h4><br>
+              <button class="btn btn-danger w-20"><a class="text-decoration-none" style="color: white" href="product.php">START ORDER</a></button>
+          </div>
+          <img class="col-sm-4"src="assets/imgs/yema.png" alt="">
+  </div>
   <!--add footer-->
-  <div>
-    <p>Indulge in the Sweet Delights of Aparri, Cagayan!</p>
+ <div class="footer">
+      <div class="row p-3">
 
-    <a href="">fb: Marias Bakeshop</a><br>
-    <a href="">ig: tres_mariascakes</a>
-  </div>
+      <div class="col-sm-3 footer-img">
+        <img src="assets/imgs/tres-marias-logo.png" alt="">
+      </div>
+
+      <div class="col-sm-4">
+        <p class="text-white">Indulge in the Sweet Delights of Aparri, Cagayan!</p>
+        <div>
+              <div class="col-sm-5 mb-2" ><img class=" navbar-brand-logo me-2" src="assets/icons/fb-logo.png" alt=""><a class="text-white text-decoration-none" href="">Marias Bakeshop</a>
+              </div>
+              <div class="col-sm-5 "><img class=" navbar-brand-logo me-2 " src="assets/icons/ig-logo.png" alt=""><a class="text-white text-decoration-none" href="">tres_mariascakes</a>
+              </div>
+      </div>
+      </div>
+      <div class="col-sm-2">
+          <p class="text-white">Explore Tres  Marias</p><br><br>
+          <a href="" class="text-white">Home</a><br>
+          <a href="" class="text-white">Products</a><br>
+          <a href="" class="text-white">Shopping Cart</a><br>
+          <a href="" class="text-white">Contact Us</a><br>
+          <a href="" class="text-white">About Us</a>
+          <a href="" class="text-white">FAQs</a>
+        </div>
+        <div class="col-sm-2">
+          <p class="text-white">Products</p><br><br>
+          <a href="" class="text-white">Baked Classics</a><br>
+          <a href="" class="text-white">Baked Loaves</a><br>
+          <a href="" class="text-white">Baked Sweets</a><br>
+          <a href="" class="text-white">Fried Breads</a><br>
+          <a href="" class="text-white">Pastries</a>
+          <a href="" class="text-white">Snack Cakes</a>
+        </div>
+      </div>
+ </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
